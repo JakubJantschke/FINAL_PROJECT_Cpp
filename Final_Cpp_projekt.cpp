5603b490203e148888c164dca6141d9418847859
JakubJantschke <jantschke14@gmail.com>
2026-05-02 11:42:46 +0200

    Udelany fuknce s grafickym nadpisem (celkove cele predelano kvuli tomu, ze predchozi kod by nemel funkce)
    Pristi tyden udelam uz MAIN

--- Final_Cpp_projekt.cpp
+++ Final_Cpp_projekt.cpp
@@ -2,57 +2,103 @@
 #include <string>
 #include <ctime>
 #include <cstdlib>
-
 using namespace std;
 
-int main() {
-    srand (time(0));
+// funkce pro vykresleni uvodniho loga
+void uvodniGrafika() {
+    cout << "########################################################" << endl;
+    cout << "#                                                      #" << endl;
+    cout << "#    _______  __   __  ______    _______  _______      #" << endl;
+    cout << "#   |       ||  | |  ||    _ |  |       ||       |     #" << endl;
+    cout << "#   |    _  ||  |_|  ||   | ||  |   _   ||_     _|     #" << endl;
+    cout << "#   |   |_| ||       ||   |_||_ |  | |  |  |   |       #" << endl;
+    cout << "#   |    ___||_     _||    __  ||  |_|  |  |   |       #" << endl;
+    cout << "#   |   |      |   |  |   |  | ||       |  |   |       #" << endl;
+    cout << "#   |___|      |___|  |___|  |_||_______|  |___|       #" << endl;
+    cout << "#                                                      #" << endl;
+    cout << "#             LEGENDA O OHNIVEM DRAKOVI                #" << endl;
+    cout << "#                                                      #" << endl;
+    cout << "########################################################" << endl;
+    cout << endl;
+    cout << "Vitej, hrdino! Tva cesta za porazkou Pyrocoila zacina..." << endl;
+    cout << "Stiskni ENTER pro vstup do sveta!" << endl;
+    cin.get(); // Pocka na stisk klavesy
+}
 
-    //PROMENNE POSTAVY
-    string jmenoClassy = "";
-    int maximalniZivoty = 0, aktualniZivoty = 0;
-    int maximalniMana = 0, aktualniMana = 0;
-    int zlato = 10, level = 1, zkusenosti = 0, hracUtok = 0;
+// FUNKCE PRO VYPOCET (RETURN)
+int secti(int a, int b) {
+    return a + b;
+}
 
-    int volbaHrace = 0;
-    bool potvrzeniClassy = false;
+// FUNKCE PRO HLÁŠKY MONSTER (VOID)
+// Tato funkce vybere náhodnou hlášku podle typu monstra
+void hradlaMonster(string jmeno) {
+    int r = rand() % 3; // náhodné èíslo 0-2
+    cout << jmeno << " krici: ";
 
-    //VOLBA POSTAVY
-    while (potvrzeniClassy == false) {
-        cout << "=== VITEJTE V TEXTOVEM RPG ===" << endl;
-        cout << "Vyberte si svoji classu (tridu):" << endl;
-        cout << "1) Paladin" << endl;
-        cout << "2) Mag" << endl;
-        cout << "3) Lovec" << endl;
-        cout << "Vase volba: ";
-        cin >> volbaHrace;
-
-        if (volbaHrace == 1) {
-            jmenoClassy = "Paladin"; maximalniZivoty = 20; maximalniMana = 10; hracUtok = 4;
-        } else if (volbaHrace == 2) {
-            jmenoClassy = "Mag"; maximalniZivoty = 12; maximalniMana = 25; hracUtok = 2;
-        } else if (volbaHrace == 3) {
-            jmenoClassy = "Lovec"; maximalniZivoty = 16; maximalniMana = 15; hracUtok = 5;
-        }
-
-        aktualniZivoty = maximalniZivoty;
-        aktualniMana = maximalniMana;
-
-        cout << "\nClass: " << jmenoClassy << endl;
-        cout << "Statistiky:" << endl;
-        cout << "  Zivot - " << aktualniZivoty << "/" << maximalniZivoty << endl;
-        cout << "  Utok  - " << hracUtok << endl;
-        cout << "  Mana  - " << aktualniMana << "/" << maximalniMana << endl;
-        cout << "\nSchopnosti:" << endl;
-        cout << "  1. Zakladni utok - Uder za " << hracUtok << " poskozeni" << endl;
-        cout << "  2. Specialni uder - Poskozeni vsem (stoji 3 many)" << endl;
-        cout << "  3. Odpocinek - Prida 5 HP (stoji 2 many)" << endl;
-
-        cout << "\nChcete potvrdit vyber teto classy? (1-Ano / 0-Zvolit jinou): ";
-        int potvrdit;
-        cin >> potvrdit;
-        if (potvrdit == 1) potvrzeniClassy = true;
-
-        return 0;
+    if (jmeno == "Vlk" || jmeno == "Divoke prase") {
+        if (r == 0) cout << "\"Vrrr... citim tvou krev!\"" << endl;
+        else if (r == 1) cout << "\"Haf! Tohle bude snadna korist.\"" << endl;
+        else cout << "*Hladove vrèí a cení zuby*" << endl;
     }
+    else if (jmeno == "Skret" || jmeno == "Loupeznik") {
+        if (r == 0) cout << "\"Dej sem zlato, nebo te rozsekam!\"" << endl;
+        else if (r == 1) cout << "\"Hele kluci, dalsi hloupy dobrodruh!\"" << endl;
+        else cout << "\"Tvoje boty se mi budou hodit!\"" << endl;
+    }
+    else if (jmeno == "Troll") {
+        if (r == 0) cout << "\"Tamaten hnufak cufak chce nasejc zlato-sutry, majznu ho palici!\"" << endl;
+        else if (r == 1) cout << "\"Uvarim si z tebe gulas!\"" << endl;
+        else cout << "\"Krupni, krupni, tvoje kosti jsou jako susenky!\"" << endl;
+    }
+    else if (jmeno == "Kostlivec" || jmeno == "Duch") {
+        if (r == 0) cout << "\"Pridáš se k nasi mrtve armade...\"" << endl;
+        else if (r == 1) cout << "\"Citim teplo tveho zivota... chci ho!\"" << endl;
+        else cout << "*Chrastí kostmi a kvílí*" << endl;
+    }
+    else if (jmeno == "Temny Mag") {
+        if (r == 0) cout << "\"Tva duše bude ma!\"" << endl;
+        else if (r == 1) cout << "\"Neznas skutecnou silu temnoty!\"" << endl;
+        else cout << "\"Tvoje magie je proti me jen hracka.\"" << endl;
+    }
+    else if (jmeno == "PYROCOIL") {
+        if (r == 0) cout << "\"VSECHNO SHORI V MEM PLAMENI!\"" << endl;
+        else if (r == 1) cout << "\"Jsi jen prach pod mými paznehty!\"" << endl;
+        else cout << "\"KONEC TVE CESTY JE TADY!\"" << endl;
+    }
+}
+
+// FUNKCE PRO VYPIS STAVU (VOID)
+void status(string jmeno, int hpM, int hpA, int mana) {
+    cout << "\n------------------------------------------------" << endl;
+    cout << " NEPØÍTEL: " << jmeno << " [HP: " << hpM << "]" << endl;
+    cout << " HRÁÈ: [HP: " << hpA << " | MANA: " << mana << "]" << endl;
+    cout << "------------------------------------------------" << endl;
+}
+
+// KONTROLA ZLATA (BOOL)
+bool maDostZlata(int hracZlato, int cena) {
+    if (hracZlato >= cena) return true;
+    cout << "Nedostatek zlata! Chybi: " << cena - hracZlato << endl;
+    return false;
+}
+
+// LEVEL UP (REFERENCE &)
+void zkontrolujLevel(int &lvl, int &xp, int &mHP, int &aHP, int &u) {
+    if (xp >= 50) {
+        lvl = secti(lvl, 1);
+        xp = 0;
+        mHP = secti(mHP, 15);
+        aHP = mHP;
+        u = secti(u, 3);
+        cout << "\n>>> LEVEL UP! Nyni uroven " << lvl << " <<<" << endl;
+    }
+}
+
+// MAIN
+int main() {
+
+
+
+    return 0;
 }
